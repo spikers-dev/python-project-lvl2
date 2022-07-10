9784a94c04dcd706057031c6252171472a0385c3
spikers-dev <spikers@inbox.com>
2022-07-10 23:28:13 +0300

    step_2

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,12 @@
+import argparse
+
+# poetry run python -m gendiff.scripts.gendiff -h
+parser = argparse.ArgumentParser(
+    description='Compares two configuration files and shows a difference.'
+                                )
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
+parser.add_argument('-f', '--format', type=str, help='set format of output')
+
+args = parser.parse_args()
+print(args.accumulate(args.first_file))
