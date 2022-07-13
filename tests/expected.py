28ea7bd25a52867f6adc0b165a2f9bfcaa816b7d
Aleksandr Shpynev <spikers@inbox.ru>
2022-07-13 22:32:30 +0300

    modified:   .gitignore 	new file:   tests/expected.py 	modified:   tests/test_gendiff.py

---
+++ tests/expected.py
@@ -0,0 +1,8 @@
+SIMPLE_STRING = '''{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}'''
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,5 +1,11 @@
-from gendiff import __version__
+from gendiff.libs.diff_flat import generate_diff
+import tests.expected as expected
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+file1 = 'tests/fixtures/file1.json'
+file2 = 'tests/fixtures/file2.json'
+
+
+def test1_simple_work():
+    actual = generate_diff(file1, file2)
+    assert actual == expected.SIMPLE_STRING
